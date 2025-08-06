676ae68ded092daf7e1545e93723d70044595370
skyrinrin <nneskygooogle@gmail.com>
2025-08-07 00:46:18 +0900

    'スケジュール化処理追加中'

--- task_app/lib/core/notification/notification_service.dart
+++ task_app/lib/core/notification/notification_service.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tzData;
 
 class NotificationService {
   static final NotificationService _instance = NotificationService._internal();
@@ -13,6 +15,8 @@ class NotificationService {
       FlutterLocalNotificationsPlugin();
 
   Future<void> init() async {
+    tzData.initializeTimeZones(); //timezoneの初期化
+
     const AndroidInitializationSettings androidInitSettings =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
@@ -34,28 +38,50 @@ class NotificationService {
     } //必要ならばIOS処理
   }
 
-  Future<void> showNotification({
+  // Future<void> showNotification({
+  //   required int id,
+  //   required String title,
+  //   required String body,
+  // }) async {
+  //   const AndroidNotificationDetails androidDetails =
+  //       AndroidNotificationDetails(
+  //         'default_channel_id',
+  //         'Default Channel',
+  //         importance: Importance.max,
+  //         priority: Priority.high,
+  //       );
+
+  //   const NotificationDetails platformDetails = NotificationDetails(
+  //     android: androidDetails,
+  //   );
+
+  //   await flutterLocalNotificationsPlugin.show(
+  //     id,
+  //     title,
+  //     body,
+  //     platformDetails,
+  //   );
+  // }
+
+  Future<void> showScheduledNotification({
     required int id,
     required String title,
     required String body,
+    required DateTime scheduledTime,
   }) async {
-    const AndroidNotificationDetails androidDetails =
-        AndroidNotificationDetails(
-          'default_channel_id',
-          'Default Channel',
-          importance: Importance.max,
-          priority: Priority.high,
-        );
-
-    const NotificationDetails platformDetails = NotificationDetails(
-      android: androidDetails,
-    );
-
-    await flutterLocalNotificationsPlugin.show(
+    await _flutterLocalNotificationsPlugin.zonedSchedule(
       id,
       title,
       body,
-      platformDetails,
+      tz.TZDateTime.from(scheduledTime, tz.local), // 必ずtzで包む
+      const NotificationDetails(
+        android: AndroidNotificationDetails('main_channel', 'Main Channel'),
+        iOS: DarwinNotificationDetails(),
+      ),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time, // 毎日同じ時刻にも可能
     );
   }
 }
